370267806c41aadd102ec1bcabfd59adf83a7f0e
mishra-rishabh <rishabh.ramg@gmail.com>
2024-01-17 00:42:43 +0530

    comma ok syntax

---
+++ 06_comma_ok_syntax/go.mod
@@ -0,0 +1,3 @@
+module comma_ok_syntax
+
+go 1.21.1
---
+++ 06_comma_ok_syntax/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	/*
+		Whatever the error will be will come into err variable.
+		Here, we haven't used err variable because it will give error if we don't use it in the code.
+		So, we have used _ (underscore) instead.
+
+		In the below commented statement, input acts as a try block and err acts as a catch block.
+	*/
+	// input, err := reader.ReadString('\n')
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Enter you name:")
+	fmt.Print("\nHello ", input)
+}
